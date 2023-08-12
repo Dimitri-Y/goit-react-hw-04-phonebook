716c8b6c642f31def7b2b727a1c3f123e36f4241
Dimitri-Y <dimoon.bond@gmail.com>
2023-08-12 14:42:01 +0300

    add hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import {useEffect,useState } from "react";
+import {useEffect, useState } from "react";
 import ContactForm from "./ContactForm/ContactForm";
 import Filter from "./Filter/Filter";
 import ContactList from "./ContactList/ContactList";
@@ -11,14 +11,6 @@ const App = () =>
       return JSON.parse(localStorage.getItem('constacts'))??[];
   });
   const [filter, setFilter] = useState('');
-  
-  useEffect(()=>{
-    const contacts=localStorage.getItem('contacts');
-    const parsedContacts=JSON.parse(contacts);
-    if(parsedContacts){
-      setContacts(parsedContacts);
-    }
-  },[]);
 
   useEffect(()=>{
       localStorage.setItem('contacts', JSON.stringify(contacts));
@@ -45,8 +37,7 @@ const App = () =>
   
   const handleDeleteContact=event=>{
     setContacts(contacts=>contacts.filter(contact=>contact.id !== event.target.id));
-  }
-  
+  }  
     return (
       <div
         style={{
@@ -64,71 +55,4 @@ const App = () =>
     );
 }
 
-// class App extends Component{
-//   state = {
-//     contacts: [],
-//     filter: '',
-//   }
-//   componentDidMount(){
-//     const contacts=localStorage.getItem('contacts');
-//     const parsedContacts=JSON.parse(contacts);
-//     if(parsedContacts){
-//       this.setState({contacts: parsedContacts});
-//     }    
-//   }
-//   componentDidUpdate(prevState, prevProps){
-//     if(this.state.contacts!==prevState.contacts){
-//       localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-//     }
-//   }
-//   onSubmit = contactData => {
-//     const { name} = contactData;
-//     const { contacts } = this.state;
-
-//     const isDuplicateName = contacts.some(
-//       contact =>
-//         contact.name.toLowerCase() === name.toLowerCase()
-//     );
-//     if (isDuplicateName) {
-//       return window.alert(
-//         `${name} is already in contacts!`
-//       );
-//     }
-//     this.setState(prevState => ({
-//       contacts: [...prevState.contacts, contactData],
-//     }));
-//   };
-//   filteredContacts=()=>{
-//     const {contacts,filter} = this.state;
-//     return contacts.filter(contact=>contact.name.toLowerCase().includes(filter.toLowerCase()))
-//   }
-//   filterChange = evt => {
-//     const { value } = evt.target;
-//     this.setState({ filter: value });
-//   };
-//   handleDeleteContact=event=>{
-//     const {contacts} =this.state;
-//     this.setState({contacts:contacts.filter(contact=>contact.id !== event.target.id)
-//   });
-//   }
-  
-//   render(){
-//     const contacts = this.filteredContacts();
-//     return (
-//       <div
-//         style={{
-//           height: '100vh',
-//           fontSize: 20,
-//           color: '#010101',
-//           padding:'16px',
-//         }}
-//       >
-//       <ContactForm  onSubmit={this.onSubmit}></ContactForm>
-//       <h2 className={css["h"]}> Contacts </h2>
-//       <Filter filterChange={this.filterChange}></Filter>
-//       <ContactList contacts={contacts} handleDeleteContact={this.handleDeleteContact}></ContactList>
-//       </div>
-//     );
-//   }
-// }
 export default App;
--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,37 +1,33 @@
-import React, { Component } from 'react';
 import css from './ContactList.module.css';
 import PropTypes from 'prop-types';
-class ContactList extends Component {
-  static propTypes = {
-    contacts: PropTypes.arrayOf(
-      PropTypes.shape({
-        id: PropTypes.string.isRequired,
-        name: PropTypes.string.isRequired,
-        number: PropTypes.string.isRequired,
-      })
-    ).isRequired,
-    handleDeleteContact: PropTypes.func.isRequired,
-  };
-  render() {
-    const { contacts, handleDeleteContact } = this.props;
-    return (
-      <ul className={css['group']}>
-        {contacts.map(contact => (
-          <li key={contact.id} className={css['list']}>
-            <p className={css['p']}>
-              {contact.name} : {contact.number}
-            </p>
-            <button
-              className={css['button']}
-              id={contact.id}
-              onClick={handleDeleteContact}
-            >
-              Delete
-            </button>
-          </li>
-        ))}
-      </ul>
-    );
-  }
-}
+const ContactList = ({ contacts, handleDeleteContact }) => {
+  return (
+    <ul className={css['group']}>
+      {contacts.map(contact => (
+        <li key={contact.id} className={css['list']}>
+          <p className={css['p']}>
+            {contact.name} : {contact.number}
+          </p>
+          <button
+            className={css['button']}
+            id={contact.id}
+            onClick={handleDeleteContact}
+          >
+            Delete
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+ContactList.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  handleDeleteContact: PropTypes.func.isRequired,
+};
 export default ContactList;
